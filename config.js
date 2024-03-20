b12429d5d5531d5d6a2677783c232e492f5712a7
TEST Ball <160318630+ishakmusk2027@users.noreply.github.com>
2024-03-20 17:27:42 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bot:  {
+    owners: ["1204876472708235270","1204876472708235270"],  // ايدي الاونر
+    botID: "1215378242773581864",  // Bot ID
+    GuildId: "1215364515365912648",   // ايدي السيرفير
+    ClientId: "1215378242773581864",    // ايدي البوت
+    serverinvte: "https://discord.gg/qmgmxrTK9W ", // انفايت سيرفر
+    clientSECRET: process.env.client, // سكريت
+    callbackURL: "https://auth113.onrender.com/login", // الكال باك
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1215378242773581864&permissions=8&scope=bot", // انفايت البوت
+    TheLinkVerfy: 'https://discord.com/oauth2/authorize?client_id=1215378242773581864&response_type=code&redirect_uri=https%3A%2F%2Fauth113.onrender.com%2Flogin&scope=identify+guilds.join+guilds', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix: 'b', 
+    ceatogry: '1215722518074097664', // كاتوجري الي يفتح فيها تكت شراء
+    TOKEN: (process.env.midd), // توكن 
+    Price: 1500,   // سعر العضو الواحد
+    TraId: '969927433165869107' // ايدي الي يتحوله كريديت
+  },
+  website: {
+    PORT: "3001",
+  }
+}
